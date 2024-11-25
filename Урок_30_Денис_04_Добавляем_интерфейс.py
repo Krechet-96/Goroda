d26eae49c9a61bce48133c37801b27e976e3cf50
Programmer <shakirov.ekb@gmail.com>
2024-11-25 20:01:03 +0500

    Добавлен ввод с помощью клавиши Enter на клавиатуре.

--- Урок_30_Денис_04_Добавляем_интерфейс.py
+++ Урок_30_Денис_04_Добавляем_интерфейс.py
@@ -17,20 +17,21 @@ def get_coordinates(city, key):
         return (f"Возникла ошибка: {e}")
 
 
-def show_coordinates():
+def show_coordinates(event=None):
     city = entry.get()
     coordinates = get_coordinates(city, key)
-    label.config(text=f"Координаты города {city}: {coordinates}")
+    label.config(text=f"Координаты города {city}:\n {coordinates}")
 
 
 key = 'b4cee9ecd4a341f485dc41cb649887cb'
 
 window=Tk()
 window.title("Координаты городов")
-window.geometry("200x100")
+window.geometry("320x100")
 
 entry = Entry()
 entry.pack()
+entry.bind("<Return>", show_coordinates)
 
 button = Button(text="Поиск координат", command=show_coordinates)
 button.pack()
