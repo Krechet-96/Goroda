f9e3ec6909cd76f5f1647ddf5343b4d706247709
Programmer <shakirov.ekb@gmail.com>
2024-11-25 20:07:27 +0500

    Добавлен вывод информации о стране и регионе города.

---
+++ Урок_30_Денис_05_Выв._инф._о_стране_и_регионе.py
@@ -0,0 +1,44 @@
+from opencage.geocoder import OpenCageGeocode
+from tkinter import *
+
+
+def get_coordinates(city, key):
+    try:
+        geocoder = OpenCageGeocode(key)
+        results = geocoder.geocode(city,languag='ru')
+        if results:
+            lat = round(results[0]['geometry']['lat'], 2) # Округляем до 2-х знаков после запятой
+            lng = round(results[0]['geometry']['lng'], 2)
+            country = results[0]['components']['country']
+            region = results[0]['components']['state']
+            return f"Широта: {lat}, Долгота: {lng} Страна: {country} Регион: {region}"
+        else:
+            return "Город не найден"
+
+    except Exception as e:
+        return (f"Возникла ошибка: {e}")
+
+
+def show_coordinates(event=None):
+    city = entry.get()
+    coordinates = get_coordinates(city, key)
+    label.config(text=f"Координаты города {city}:\n {coordinates}")
+
+
+key = 'b4cee9ecd4a341f485dc41cb649887cb'
+
+window=Tk()
+window.title("Координаты городов")
+window.geometry("320x100")
+
+entry = Entry()
+entry.pack()
+entry.bind("<Return>", show_coordinates)
+
+button = Button(text="Поиск координат", command=show_coordinates)
+button.pack()
+
+label = Label(text="Введите город и нажмите на кнопку")
+label.pack()
+
+window.mainloop()
